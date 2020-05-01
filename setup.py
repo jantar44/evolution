9f42f3866b5624b956d309e61a4cbc00cfaec6b1
jantar44 <niemno90@op.pl>
2020-05-01 22:53:24 +0200

    setup.py initial update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='evolution',
+   version='0.1',
+   description='Simulation of the evolution',
+   author='Me',
+   packages=['evolution'],
+   install_requires=['random', 'pprint'],
